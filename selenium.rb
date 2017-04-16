c0ca52bd5a1447e09506ec836bb8707b69ac7d49
tjgadm <s11118tm@gmail.com>
2017-04-16 18:19:11 +0900

    Update selenium.rb

--- selenium.rb
+++ selenium.rb
@@ -2,7 +2,7 @@ require 'headless'
 require 'selenium-webdriver'
  
 Headless.ly do
-  driver = Selenium::WebDriver.for :firefox
+  driver = Selenium::WebDriver.for :chrome
   driver.navigate.to 'http://google.com'
   puts driver.title
 end
