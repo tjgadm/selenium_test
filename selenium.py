b54bfe9ff706afbc3b1156cc5099a6c8c7f76619
tjgadm <s11118tm@gmail.com>
2017-04-16 17:40:29 +0900

    Update selenium.py

--- selenium.py
+++ selenium.py
@@ -1,6 +1,6 @@
-import selenium
+#import selenium
 from time import sleep
-browser = selenium.webdriver.firefox()
-browser.get('http://google.com')
+#browser = selenium.webdriver.firefox()
+#browser.get('http://google.com')
 sleep(10)
-browser.close()
+#browser.close()
