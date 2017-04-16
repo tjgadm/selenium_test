34e8954396afebff5923ece8b229a4a70ef0ba64
tjgadm <s11118tm@gmail.com>
2017-04-16 17:53:25 +0900

    Update selenium.py

--- selenium.py
+++ selenium.py
@@ -1,4 +1,4 @@
-from selenium import *
+from selenium.webdriver.firefox.webdriver import WebDriver as webdriver
 from time import sleep
 browser = webdriver.firefox()
 #browser.get('http://google.com')
